fdb9b932cac14abbf4b07d84f6a46a21da20c4cc
Rushi Vishavadia <rushi.v@gmail.com>
2017-06-01 12:01:29 +0530

    Fix minor bug -- check array key exists

--- web/fetch.php
+++ web/fetch.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/../src/SpoolReader.php';
 require_once dirname(__FILE__) . '/../config/config.php';
 
 $spoolReader = new SpoolReader(SPOOL_DIR);
-if ($_GET['clear'] == 1) {
+if (isset($_GET['clear']) && $_GET['clear'] == 1) {
     $spoolReader->clear();
 }
 $messages = $spoolReader->run();
