55dd8d258434b35bd9252a78b20deb23464724bf
Rushi Vishavadia <rushi.v@gmail.com>
2020-06-05 11:47:22 +0530

    Refactor & Re-design (#6)

--- web/index.php
+++ web/index.php
@@ -2,113 +2,67 @@
 require_once '../config/config.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html class="h-100">
 <head>
     <title>Swiftmailer Spool Reader</title>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <link href="css/bootstrap.css" rel="stylesheet"/>
-    <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
-    <script type="text/javascript" src="js/moment.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <style type="text/css">
-        @import url("https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700&display=swap");
-        html, body {
-            font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
-        }
-        #main {
-            padding: 0 50px;
-        }
-        .message-date {
-            width: 170px;
-        }
-        .message-additional-header {
-            font-size: 12px;
-            margin-top: 3px;
-        }
-        .message-additional-header .field-name {
-            margin-right: 3px;
-        }
-        .message-additional-header .field-name:after {
-            content: ':';
-        }
-        .message-actions {
-            vertical-align: middle;
-            text-align: right;
-        }
-        .modal {
-            width: 1024px;
-            margin-left : 120px;
-            overflow-y: hidden;
-        }
-        .modal-content {
-            width: 800px;
-        }
-        .modal-body {
-            padding: 0px;
-            padding-bottom: 20px;
-        }
-        .modal-body iframe {
-            border: 0px;
-            width: 100%;
-            padding: 10px;
-            min-height: 500px;
-        }
-        :focus {
-            outline: 0 !important;
-        }
-        .total-messages {
-            font-weight: bold;
-        }
-        .action-clear, .action-fetch {
-            margin-left: 10px;
-        }
-    </style>
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
+    <link href="css/app.css" rel="stylesheet"/>
+    <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="../node_modules/moment/moment.js"></script>
+    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="../node_modules/mithril/mithril.min.js"></script>
 </head>
-<body>
-<div id="main" class="container-fluid">
-    <div class="row">
-        <div class="col-xs-12">
-            <h2>Spool Reader</h2>
+<body class="d-flex flex-column h-100">
+    <div class="main container-fluid">
+        <div class="row">
+            <div class="col">
+                <h2>Swiftmailer Spool Reader</h2>
+            </div>
+        </div>
+        <div class="row py-2">
+            <div class="col">
+                Found <span class="total-messages">0</span> spooled emails in in <code><?php echo SPOOL_DIR;?></code>
+            </div>
+            <div class="col">
+                <button type='button' class='btn btn-sm btn-danger float-right action-clear'>
+                    <svg class="bi bi-trash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"/>
+                    </svg>
+                    Clear Spool
+                </button>
+                <button type='button' class='btn btn-sm btn-primary float-right action-fetch'>
+                    <svg class="bi bi-arrow-clockwise" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M3.17 6.706a5 5 0 017.103-3.16.5.5 0 10.454-.892A6 6 0 1013.455 5.5a.5.5 0 00-.91.417 5 5 0 11-9.375.789z" clip-rule="evenodd"/>
+                        <path fill-rule="evenodd" d="M8.147.146a.5.5 0 01.707 0l2.5 2.5a.5.5 0 010 .708l-2.5 2.5a.5.5 0 11-.707-.708L10.293 3 8.147.854a.5.5 0 010-.708z" clip-rule="evenodd"/>
+                    </svg>
+                    Refresh
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="px-3 py-4 col">
+                <table class="table table-striped table-hover messages"></table>
+            </div>
         </div>
     </div>
-    <div class="row" style='margin-top: 20px;'>
-        <div class="col-xs-6">
-            Found <span class="total-messages">0</span> spooled emails in in <code><?php echo SPOOL_DIR;?></code>
-        </div>
-        <div class="col-xs-6">
-            <button type='button' class='btn btn-sm btn-danger pull-right action-clear'>Clear Spool</button>
-            &nbsp;&nbsp;
-            <button type='button' class='btn btn-sm btn-primary pull-right action-fetch'>Refresh</button>            
-        </div>
-    </div>
-    <div class="row" style='margin-top: 20px;'>
-        <div class="col-xs-12">
-            <table class="table table-striped table-hover messages">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Date</th>
-                <th>From</th>
-                <th>Reply To</th>
-                <th>To</th>
-                <th>Subject</th>
-                <th class='text-right'>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr class="loading">
-                <td colspan="7">Loading...</td>
-            </tr>
-            </tbody>
-        </table>
-            <div id="modalHolder"></div>
-        </div>
-    </div>
-</div>
 
-<script type="text/javascript" src="js/main.js"></script>
+    <footer class="footer mt-auto py-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    Last refreshed: <span class="last-refreshed"></span>
+                </div>
+                <div class="col">
+                    <div class="float-right">
+                        <svg viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+                        <a target="_blank" href="https://github.com/rushi/swiftmailer-spoolreader">Fork me on Github</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
+    <script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
---
+++ web/js/app.js
@@ -0,0 +1,134 @@
+const Messages = {
+    list: [],
+    lastRefreshed: moment(),
+    fetch: (vnode, params = {}) => m.request({url: "fetch.php", params}).then((result) => {
+        Messages.lastRefreshed = moment();
+        Messages.list = result;
+    }),
+}
+
+// Hack to inject HTML into the body of an iFrame
+const proxy = vnode => m.render(vnode.dom.contentDocument.documentElement, vnode.children);
+const IframeNode = {
+    oncreate: proxy,
+    onupdate: proxy,
+    view: vnode => m('iframe', vnode.attrs)
+}
+
+const EmailRow = {
+
+    formatEmail: (email, abbr = false) => {
+        if (typeof email === 'string') {
+            return email;
+        }
+
+        const address = Object.keys(email)[0];
+        const name = Object.values(email)[0];
+        if (abbr) {
+            return name ? m('abbr', {rel: 'tooltip', title: address}, name) : m('span', address);
+        } else {
+            return name ? m('span.email', [name, m('small', address)]) : m('span', address);
+        }
+    },
+
+    renderTo: function (headers) {
+        if (headers['X-Swift-To']) {
+            let xSwiftTo = headers['X-Swift-To'];
+            const address = Object.keys(xSwiftTo)[0];
+            const name = Object.values(xSwiftTo)[0];
+            return m('span.xswift', [name, m('small', address), this.formatEmail(headers['To'], true)]);
+        }
+
+        return this.formatEmail(headers['To']);
+    },
+
+    modal: function(message) {
+        const headers = message.headers;
+        const messageId = headers['Message-ID'][0].replace(/@.*$/,'');
+
+        const modalHeader = m("h6.modal-title.float-left", headers['Subject']);
+        const button = m("button", {type: "button", class: "close", "data-dismiss": "modal"}, "×");
+        const modalInfo = m('.row.py-1', [
+            m('.col-1.label', 'From'), m('.col-5.value', this.formatEmail(headers['From'])),
+            m('.col-1.label', 'To'), m('.col-5.value', this.renderTo(headers))
+        ]);
+
+        return m(".modal", {id: `modal-${messageId}`}, [
+            m(".modal-dialog.modal-dialog-scrollable", [
+                m(".modal-content", [
+                    m(".modal-header", [
+                        m('.container-fluid', [
+                            m('.row', m('.col.px-0', [modalHeader, button])),
+                            modalInfo
+                        ])
+                    ]),
+                    m(".modal-body", m(IframeNode, {onload: {}, id: `iframe-${messageId}`}, m.trust(message.body)))
+                ])
+            ])
+        ]);
+    },
+
+    btnShowEmail: (message) => {
+        const messageId = message.headers['Message-ID'][0].replace(/@.*$/,'');
+        const props = {type: "button", "data-toggle": "modal", "data-target": `#modal-${messageId}`, class: "btn btn-sm btn-primary"};
+        return m("button", props, "Show Email");
+    },
+
+    formatDate: (unixtime) => {
+        const date = moment(unixtime * 1000);
+        let dateStr = date.format('MMM D HH:MM');
+        if (date.format('l') === moment().format('l')) {
+            dateStr = 'Today ' + date.format('HH:MM');
+        }
+        return m("span.date", [dateStr, m('small.relative-date', date.fromNow())]);
+    },
+
+    view: function (vnode) {
+        // console.log('Rendering EmailRow', vnode.attrs);
+        const data = vnode.attrs;
+        const headers = data.message.headers;
+        const replyTo = headers['Reply-To'] ? this.formatEmail(headers['Reply-To']) : m('span', 'N/A');
+
+        return [m("tr", [
+            m("td", data.idx),
+            m("td", this.formatDate(headers['Date'])),
+            m("td", this.formatEmail(headers['From'])),
+            m("td", replyTo),
+            m("td", this.renderTo(headers)),
+            m("td", headers['Subject']),
+            m("td", this.btnShowEmail(data.message))
+        ]), this.modal(data.message)];
+    }
+}
+
+const EmailList = {
+    oninit: Messages.fetch,
+    data: {idx: 0}, // Initial state
+
+    oncreate: function(vnode) {
+        setInterval(m.redraw, 30000);
+    },
+
+    view: function (vnode) {
+        vnode.state.data.idx = 0; // Reset count to zero when the list is rendered
+        const count = Messages.list.length;
+        const title = document.title.replace(/^\(\d+\) /, '');
+        document.title = `(${count}) ${title}`;
+        $('.total-messages').html(count);
+        $('.last-refreshed').html(Messages.lastRefreshed.fromNow());
+
+        const thead = m("thead", [
+            m("tr", [
+                m("th", "#"), m("th.date", "Date"), m("th.email", "From"), m("th.email", "Reply To"),
+                m("th.email", "To"), m("th", "Subject"), m("th.actions", "Actions")
+            ])
+        ]);
+
+        const tbody = m("tbody", Messages.list.map((message) => m(EmailRow, {message, idx: ++vnode.state.data.idx})));
+        return [thead, tbody];
+    }
+}
+
+m.mount(document.querySelector('table'), EmailList);
+$('.action-fetch').on('click', Messages.fetch);
+$('.action-clear').on('click', Messages.fetch.bind(this, null, {clear: 1}));
